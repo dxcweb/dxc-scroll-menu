3136cfac1627c0847e40a74684260368c50acef1
郭威 <dxcweb2017@gmail.com>
2021-05-11 16:49:23 +0800

    格式化

--- src/InnerWrapper.js
+++ src/InnerWrapper.js
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from "../assets/InnerWrapper.css";
-import classNames from "classnames/bind";
-import Block from "dxc-flex";
-import { getClientRect } from "./utils";
-import Items from "./Items";
+import React from 'react';
+import styles from '../assets/InnerWrapper.css';
+import classNames from 'classnames/bind';
+import Block from 'dxc-flex';
+import { getClientRect } from './utils';
+import Items from './Items';
 const cx = classNames.bind(styles);
 class InnerWrapper extends React.PureComponent {
   state = {
@@ -22,10 +22,10 @@ class InnerWrapper extends React.PureComponent {
     let transition = 0.1;
     const wrapperStyles = {
       transform: `translate3d(${translate}px, 0, 0)`,
-      transition: `transform ${dragging || !mounted ? "0" : transition}s  ease-out`,
+      transition: `transform ${dragging || !mounted ? '0' : transition}s  ease-out`,
     };
     return (
-      <Block className={cx("innerWrapperClass")} style={wrapperStyles} ref={(inst) => this.setMenuInnerRef(inst)}>
+      <Block className={cx('innerWrapperClass')} style={wrapperStyles} ref={(inst) => this.setMenuInnerRef(inst)}>
         <Items selected={selected} renderItem={renderItem} data={data} setItemRef={setItemRef} />
       </Block>
     );
